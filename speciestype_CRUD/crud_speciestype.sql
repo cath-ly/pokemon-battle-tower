a657971328ac776527301bdfa4248b5a3f350931
chantakrak.ath-ly <chantakrak.ath-ly@centre.edu>
2021-11-03 22:16:55 +0000

    adjusted tings with crud sqls

---
+++ speciestype_CRUD/crud_speciestype.sql
@@ -0,0 +1,6 @@
+SOURCE create_speciestype.sql;
+SOURCE add_speciestype.sql;
+SOURCE select_speciestype.sql;
+SOURCE view_speciestype.sql;
+SOURCE delete_speciestype.sql;
+SOURCE update_speciestype.sql;
