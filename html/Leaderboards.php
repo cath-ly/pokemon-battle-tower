0de41321fb41b34e5e2afa3e92121323340835fe
xxtakrakxx <chantakrak.ath-ly@centre.edu>
2021-11-28 21:52:58 +0000

    adding Nicks php

---
+++ html/Leaderboards.php
@@ -0,0 +1,51 @@
+<?php 
+    ini_set('display errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    /*need to connect to DB*/ 
+    $config = parse_ini_file('/home/nalicea4242/mysqli.ini');
+    $dbname = 'temp';
+
+    /*config shit*/
+    $conn = new mysqli(
+        $config['mysqli.default_host'],
+        $config['mysqli.default_user'],
+        $config['mysqli.default_pw'],
+        $dbname);
+
+    if (!$conn)
+    {
+        echo "Error: Connection to MySQL could not be made." . "<br>";
+        echo "Errno: $conn->connect_errno; i.e. $conn->connect_error \n";
+        exit;
+    }
+
+    //tbl view query
+
+    $query = "SELECT * FROM Leaderboard;";
+    $result = $conn->query($query);
+    if(!$result){
+        echo "query failed";
+    }
+
+    $rows = $result->fetch_all();
+    $spec_row = $result->num_rows;
+
+    <form action="viewLeaderboard.php" method=POST>
+    <p>
+        <table>
+            <thead>
+            <tr>
+                <th> "Leaderboards"</th>
+                <?php while ($field = $result->fetch_field()){?>
+                    <td><?php echo $field->name; ?> </td>
+                <?php } ?>
+            </tr>
+            </thead>
+        </table>
+        <input type 
+        </form>
+    </p>
+    <?php }
+?>
