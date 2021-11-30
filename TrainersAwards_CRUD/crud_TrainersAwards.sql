bd7769926834d0c8287409cb69eb3eea7ace7f8b
Eli Gooch <eli.gooch@centre.edu>
2021-11-30 23:59:50 +0000

    main.sql and additional crud files

---
+++ TrainersAwards_CRUD/crud_TrainersAwards.sql
@@ -0,0 +1,5 @@
+SOURCE create_TrainersAwards.sql;
+SOURCE select_TrainersAwards.sql;
+SOURCE add_TrainersAwards.sql;
+SOURCE update_TrainersAwards.sql;
+SOURCE delete_TrainersAwards.sql;
---
+++ Trainers_CRUD/crud_trainer.sql
@@ -0,0 +1,4 @@
+SOURCE create_trainer.sql;
+SOURCE select_trainer.sql;
+SOURCE add_trainer.sql;
+SOURCE delete_trainer.sql;
