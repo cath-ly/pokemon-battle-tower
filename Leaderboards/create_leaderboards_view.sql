91c6f4d035de75bc7cc80a18e29e921f9fd94f4e
NickA42 <nicholas.alicea@centre.edu>
2021-12-01 18:46:21 -0500

    added delim

--- Leaderboards/create_leaderboards_view.sql
+++ Leaderboards/create_leaderboards_view.sql
@@ -1,6 +1,7 @@
 -- Leaderboard creation script
  
 -- find most recent loss
+DELIMITTER //
 CREATE FUNCTION most_recent_loss(trainer_id INT)
 RETURN TYPE DATE
 RETURN(
@@ -8,7 +9,7 @@ RETURN(
     ON trainerID = trainer_1 OR trainer_id = trainer_2 
     WHERE trainer_id != winner;
 );
-CALL most_recent_loss(trainer_id);
+//
 
 -- count wins until most recent loss
 CREATE FUNCTION count_wins(trainer_id INT, most_recent_loss TIMESTAMP)
@@ -18,7 +19,7 @@ RETURN(
     SELECT COUNT(winner) AS winstreak FROM trainer_battles
     WHERE trainer_id = trainer_1 OR trainer_id = trainer_2 AND trainer_id = Winner AND battle_date > most_recent_loss;
 );
-CALL count_wins(trainer_id, most_recent_loss);
+//
 
 -- count number of awards for a given trainer
 CREATE FUNCTION count_awards(trainer_id) 
@@ -26,6 +27,10 @@ RETURN TYPE INT
 RETURN(
     SELECT COUNT(trainer_id) AS number_of_awards FROM trainer_awards;
 );
+//
+
+CALL most_recent_loss(trainer_id);
+CALL count_wins(trainer_id, most_recent_loss);
 CALL count_awards(trainer_id);
 
 -- create view/do i add this into the query for the viewLeaderboards.php?
@@ -35,3 +40,4 @@ FROM trainers
 ORDER BY winstreak DESC, number_of_awards DESC;
 
 SELECT * FROM leaderboard;
+
