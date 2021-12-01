603e43ea0d1aeea5b7b452ab6730ca32897d241d
Eli Gooch <eli.gooch@centre.edu>
2021-12-01 23:41:22 +0000

    index.php and main.sql

--- html/index.php
+++ html/index.php
@@ -56,7 +56,7 @@
         $dbhost = 'localhost';
         $dbuser = 'webuser';
         $dbpass = 'joe';
-        $database = 'battle_towers_pers'; 
+        $database = 'battle_tower_pers'; 
 
 
         $current_time = microtime(true);
