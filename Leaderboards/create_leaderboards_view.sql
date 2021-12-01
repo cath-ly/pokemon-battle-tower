b7e3eba1db7760ab6be58d7853c9e4e35ae43161
NickA42 <nicholas.alicea@centre.edu>
2021-12-01 18:57:19 -0500

    meh x3

--- Leaderboards/create_leaderboards_view.sql
+++ Leaderboards/create_leaderboards_view.sql
@@ -3,8 +3,8 @@
 -- find most recent loss
 DELIMITER //
 CREATE FUNCTION most_recent_loss(trainer_id INT)
-RETURN DATE
-RETURN(
+RETURNs DATE
+RETURN (
     SELECT battle_date FROM trainer_battles 
     ON trainerID = trainer_1 OR trainer_id = trainer_2 
     WHERE trainer_id != winner;
@@ -13,8 +13,8 @@ RETURN(
 
 -- count wins until most recent loss
 CREATE FUNCTION count_wins(trainer_id INT, most_recent_loss DATE)
-RETURN INT 
-RETURN(
+RETURNs INT 
+RETURN (
     -- how do i stop it from counting past their most recent loss? 
     SELECT COUNT(winner) AS winstreak FROM trainer_battles
     WHERE trainer_id = trainer_1 OR trainer_id = trainer_2 AND trainer_id = Winner AND battle_date > most_recent_loss;
@@ -23,8 +23,8 @@ RETURN(
 
 -- count number of awards for a given trainer
 CREATE FUNCTION count_awards(trainer_id) 
-RETURN INT
-RETURN(
+RETURNs INT
+RETURN (
     SELECT COUNT(trainer_id) AS number_of_awards FROM trainer_awards;
 );
 //
