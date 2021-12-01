501852b3a0262d662ff3700468e627ed519f2e88
NickA42 <nicholas.alicea@centre.edu>
2021-12-01 18:31:21 -0500

    edited leaderboards

--- Leaderboards/create_leaderboards_view.sql
+++ Leaderboards/create_leaderboards_view.sql
@@ -8,6 +8,7 @@ RETURN(
     ON trainerID = trainer_1 OR trainer_id = trainer_2 
     WHERE trainer_id != winner;
 );
+CALL most_recent_loss(trainer_id);
 
 -- count wins until most recent loss
 CREATE FUNCTION count_wins(trainer_id INT, most_recent_loss TIMESTAMP)
@@ -17,16 +18,20 @@ RETURN(
     SELECT COUNT(Winner) AS winstreak FROM trainer_battles
     WHERE trainer_id = trainer_1 OR trainer_id = trainer_2 AND trainer_id = Winner AND battle_date > most_recent_loss;
 );
+CALL count_wins(trainer_id, most_recent_loss);
 
 -- count number of awards for a given trainer
 CREATE FUNCTION count_awards(trainer_id) 
 RETURN TYPE INT
 RETURN(
-    SELECT count(trainer_id) AS number_of_awards FROM trainer_awards;
+    SELECT COUNT(trainer_id) AS number_of_awards FROM trainer_awards;
 );
+CALL count_awards(trainer_id);
 
--- create view 
+-- create view/do i add this into the query for the viewLeaderboards.php?
 CREATE VIEW [leaderboard] AS 
 SELECT trainer_id, count_wins(trainer_id) AS winstreak, count_awards(trainer_id) AS number_of_awards--calc ranking in php
 FROM trainers
 ORDER BY winstreak DESC, number_of_awards DESC;
+
+SELECT * FROM leaderboard;
