744bc8d482452eb45ddfc565686c759730e079a4
NickA42 <nicholas.alicea@centre.edu>
2021-12-02 15:29:48 -0500

    meh x4

--- Trainer_Battles_CRUD/create_trainer_battles.sql
+++ Trainer_Battles_CRUD/create_trainer_battles.sql
@@ -4,8 +4,8 @@ CREATE TABLE trainer_battles(
     trainer_1 VARCHAR (30), 
     trainer_2 VARCHAR (30), 
     winner VARCHAR (20),
+    battle_date DATE,
     FOREIGN KEY (trainer_1) REFERENCES trainers(trainer_name),
     FOREIGN KEY (trainer_2) REFERENCES trainers(trainer_name), 
-    FOREIGN KEY (winner) REFERENCES trainers(trainer_name),
-    battle_date DATE  
+    FOREIGN KEY (winner) REFERENCES trainers(trainer_name) 
 );
