d6183afe796794466558bfbdf8327c01697ff063
Eli Gooch <eli.gooch@centre.edu>
2021-12-02 01:01:34 +0000

    sql debugging, renamed select_pokemove ;

--- PokeMove_CRUD/update_pokemove.sql
+++ PokeMove_CRUD/update_pokemove.sql
@@ -1,5 +1,5 @@
-UPDATE poke_move SET 
-... = [NULL|DEFAULT|<value_expression>],
-... = [NULL|DEFAULT|<value_expression>],
-...
-WHERE poke_id = ???;
+-- UPDATE poke_move SET 
+-- ... = [NULL|DEFAULT|<value_expression>],
+-- ... = [NULL|DEFAULT|<value_expression>],
+-- ...
+-- WHERE poke_id = ???;
--- Pokemon_CRUD/update_pokemon.sql
+++ Pokemon_CRUD/update_pokemon.sql
@@ -1,3 +1,3 @@
-UPDATE pokemon;
-SET poke_level = poke_level + 1;
+UPDATE pokemon
+SET poke_level = poke_level + 1,
 WHERE poke_id = <insert value>;
