97d135a4ed0384a666bb1caed195259b98fd2602
NickA42 <nicholas.alicea@centre.edu>
2021-12-02 15:25:34 -0500

    another one - dj fatboy

--- Trainer_Battles_CRUD/create_trainer_battles.sql
+++ Trainer_Battles_CRUD/create_trainer_battles.sql
@@ -3,7 +3,7 @@ CREATE TABLE trainer_battles(
     battle_id INT AUTO_INCREMENT,
     trainer_1 VARCHAR (30), 
     trainer_2 VARCHAR (30), 
-    winner INT (20),
+    winner VARCHAR (20),
     FOREIGN KEY (trainer_1) REFERENCES trainers(trainer_name),
     FOREIGN KEY (trainer_2) REFERENCES trainers(trainer_name), 
     FOREIGN KEY (winner) REFERENCES trainers(trainer_name),
