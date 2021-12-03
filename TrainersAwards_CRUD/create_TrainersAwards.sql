0d9e8dfd5cafc20621a4ae4696be3a3f2011e613
NickA42 <nicholas.alicea@centre.edu>
2021-12-02 23:24:48 -0500

    fixed foreign key stuff

--- TrainersAwards_CRUD/create_TrainersAwards.sql
+++ TrainersAwards_CRUD/create_TrainersAwards.sql
@@ -2,6 +2,6 @@ CREATE TABLE trainer_awards(
 	PRIMARY KEY (trainer_id, award_name), 
 	trainer_id INT,
 	award_name VARCHAR(12),
-	FOREIGN KEY (trainer_id) REFERENCES trainer_id(trainers), 
-	FOREIGN KEY (award_name) REFERENCES award_name(awards)
+	FOREIGN KEY (trainer_id) REFERENCES trainers(trainer_id), 
+	FOREIGN KEY (award_name) REFERENCES awards(award_name)
 );
