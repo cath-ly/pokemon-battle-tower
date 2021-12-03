ecfb15f59a8cd1dc9c23b6506b5fdf310062a7a3
NickA42 <nicholas.alicea@centre.edu>
2021-12-02 20:15:59 -0500

    more minor edits to sql

--- Pokemon_CRUD/create_pokemon.sql
+++ Pokemon_CRUD/create_pokemon.sql
@@ -1,11 +1,11 @@
 CREATE TABLE pokemon (
+    PRIMARY KEY (poke_id),
     poke_id INT AUTO_INCREMENT,
     poke_name VARCHAR(20),
     poke_trait VARCHAR(20),
     poke_found VARCHAR(20),
     mega BOOLEAN,
     poke_level INT(3),
-    PRIMARY KEY (poke_id),
     species_name VARCHAR(11),
     FOREIGN KEY (species_name) REFERENCES species(species_name)
 );
