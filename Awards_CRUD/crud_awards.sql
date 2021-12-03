060ef89562c9484aa0e436c7c465533ef920bc4b
NickA42 <nicholas.alicea@centre.edu>
2021-12-02 19:29:34 -0500

    minor changes

---
+++ Awards_CRUD/crud_awards.sql
@@ -0,0 +1,5 @@
+SOURCE add_awards.sql;
+SOURCE create_awards.sql;
+SOURCE del_Awards.sql;
+SOURCE select_award.sql;
+SOURCE view_Awards.sql;
--- main.sql
+++ main.sql
@@ -11,12 +11,11 @@ SOURCE typead_CRUD/crud_typead.sql;
 SOURCE typedisad_CRUD/crud_typedisad.sql;
 SOURCE Species_CRUD/crud_species.sql;
 SOURCE speciestype_CRUD/crud_speciestype.sql;
-
 SOURCE Trainers_CRUD/crud_trainer.sql;
 SOURCE Pokemon_CRUD/crud_pokemon.sql;
 SOURCE PokeMove_CRUD/crud_pokemove.sql;
 SOURCE Trainer_Battles_CRUD/crud_trainer_battles.sql;
-
+SOURCE Awards_CRUD/crud_awards.sql;
 SOURCE Trainers_Pokemon/crud_trainers_pokemon.sql;
 SOURCE TrainersAwards_CRUD/crud_TrainersAwards.sql;
 SOURCE Leaderboards/create_leaderboards_view.sql;
