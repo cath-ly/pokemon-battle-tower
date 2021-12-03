2c73e86c2c0f56cdd565fd8a678b5d6b700e8c2d
xxtakrakxx <chantakrak.ath-ly@centre.edu>
2021-12-03 05:45:15 +0000

    fixed somewhat of the select

--- Trainer_Battles_CRUD/select_trainer_battles.sql
+++ Trainer_Battles_CRUD/select_trainer_battles.sql
@@ -1,5 +1,12 @@
-SELECT * FROM trainer_battles
+-- try this out and see how it fairs
+SELECT winner, trainer_name, battle_date FROM trainer_battles
+    INNER JOIN trainers
+    ON (trainer_battles.winner = trainers.trainer_id)
+-- GROUP BY trainer_name
+ORDER BY battle_date DESC;
+
+/*SELECT * FROM trainer_battles
     INNER JOIN trainers
     ON trainer_battles.trainer_1 = trainers.trainer_id AND trainer_battles.trainer_2 = trainers.trainer_id AND trainer_battles.winner = trainers.trainer_id
 GROUP BY trainer_name
-ORDER BY battle_date DESC;
+ORDER BY battle_date DESC;*/
