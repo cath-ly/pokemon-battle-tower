5ee92d2fd73ea2d94d7ff7ef03fafe7d7ee8130f
NickA42 <nicholas.alicea@centre.edu>
2021-12-02 23:17:41 -0500

    testing

--- Leaderboards/create_leaderboards_view.sql
+++ Leaderboards/create_leaderboards_view.sql
@@ -37,7 +37,7 @@ RETURN (
 -- create view/do i add this into the query for the viewLeaderboards.php?
 CREATE VIEW leaderboard AS
 SELECT trainer_id, count_wins(trainer_id) AS winstreak, count_awards(trainer_id) AS number_of_awards
-FROM trainer_battles
+FROM trainers AND trainer_battles
 ORDER BY winstreak DESC, number_of_awards DESC;
 
 SELECT * FROM leaderboard;
