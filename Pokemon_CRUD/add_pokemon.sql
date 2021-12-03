6a61dc82b790f0cc0e255236a033133dc5f9cc92
xxtakrakxx <chantakrak.ath-ly@centre.edu>
2021-12-03 05:30:39 +0000

    fixed pokemon structure

--- Pokemon_CRUD/add_pokemon.sql
+++ Pokemon_CRUD/add_pokemon.sql
@@ -1,5 +1,6 @@
-INSERT INTO pokemon (poke_name, poke_trait, poke_found, mega, poke_level, species_name)
-VALUES  ('Pikachu', 'Brave', 'Bushes', 1, 10, 'Pikachu'), 
+INSERT INTO pokemon (species_name, poke_name, poke_trait, poke_found, mega, poke_level)
+VALUES  ('Pikachu', 'Pikachu', 'Brave', 'Bushes', 1, 10);
+/* 
         ('Squirtle', 'Quirky', 'Bushes', 1, 7, 'Squirtle'), 
         ('Charmander', 'Snappy', 'Bushes', 1, 11, 'Charmander'), 
         ('Bulbasaur', 'Brave', 'Bushes', 1, 9, 'Bulbasaur'), 
@@ -10,4 +11,4 @@ VALUES  ('Pikachu', 'Brave', 'Bushes', 1, 10, 'Pikachu'),
         ('Bulbasaur', 'Brave', 'Bushes', 1 10, 'Bulbasaur'), 
         ('Meowth', 'Quirky', 'Rocket', 0, 11, 'Meowth'), 
         ('Pikachu', 'Brave', 'Bushes', 1, 10, 'Pikachu'), 
-        ('Squirtle', 'Quirky', 'Bushes', 1, 7, 'Squirtle');
+        ('Squirtle', 'Quirky', 'Bushes', 1, 7, 'Squirtle');*/
