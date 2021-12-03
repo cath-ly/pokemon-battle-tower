4bb99d88657fd0bad6db9138264071da057b75f4
NickA42 <nicholas.alicea@centre.edu>
2021-12-03 02:00:40 -0500

    implemented deletion rule for trainers

--- Trainers_Pokemon/create_trainers_pokemon.sql
+++ Trainers_Pokemon/create_trainers_pokemon.sql
@@ -5,3 +5,5 @@ CREATE TABLE trainers_pokemon(
     FOREIGN KEY (trainer_id) REFERENCES trainers(trainer_id), 
     FOREIGN KEY (poke_id) REFERENCES pokemon(poke_id)
 );
+
+ALTER TABLE battle_towers_pers.trainers_pokemon WITH CHECK ADD CONSTRAINT trainers_pokemon.trainer_id FOREIGN KEY (trainer_id) REFERENCES battle_towers_pers.trainers(trainer_id) ON DELETE CASCADE;
