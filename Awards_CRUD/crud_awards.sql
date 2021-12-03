6dbf34fb4d4dbb1321acc02413eec50a85593cf9
NickA42 <nicholas.alicea@centre.edu>
2021-12-02 19:51:57 -0500

    changes - black sabbath

--- Awards_CRUD/crud_awards.sql
+++ Awards_CRUD/crud_awards.sql
@@ -1,5 +1,5 @@
-SOURCE Awards_CRUD/add_awards.sql;
 SOURCE Awards_CRUD/create_award.sql;
+SOURCE Awards_CRUD/add_awards.sql;
 SOURCE Awards_CRUD/del_Awards.sql;
 SOURCE Awards_CRUD/select_award.sql;
 SOURCE Awards_CRUD/view_Awards.sql;
--- main.sql
+++ main.sql
@@ -5,6 +5,7 @@ DROP DATABASE IF EXISTS battle_towers_pers;
 CREATE DATABASE battle_towers_pers;
 USE battle_towers_pers;
 
+SOURCE Awards_CRUD/crud_awards.sql;
 SOURCE TypeList_CRUD/crud_typelist.sql;
 SOURCE MoveList_CRUD/crud_movelist.sql;
 SOURCE typead_CRUD/crud_typead.sql;
@@ -15,7 +16,6 @@ SOURCE Trainers_CRUD/crud_trainer.sql;
 SOURCE Pokemon_CRUD/crud_pokemon.sql;
 SOURCE PokeMove_CRUD/crud_pokemove.sql;
 SOURCE Trainer_Battles_CRUD/crud_trainer_battles.sql;
-SOURCE Awards_CRUD/crud_awards.sql;
 SOURCE Trainers_Pokemon/crud_trainers_pokemon.sql;
 SOURCE TrainersAwards_CRUD/crud_TrainersAwards.sql;
 SOURCE Leaderboards/create_leaderboards_view.sql;
