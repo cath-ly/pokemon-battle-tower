47601a5e47e6c557b0008f0183d18afc1a3a7177
NickA42 <nicholas.alicea@centre.edu>
2021-12-03 00:30:56 -0500

    edited the inner join

--- Trainer_Battles_CRUD/select_trainer_battles.sql
+++ Trainer_Battles_CRUD/select_trainer_battles.sql
@@ -1,5 +1,5 @@
 SELECT * FROM trainer_battles
     INNER JOIN trainers
-    ON trainer_battles.trainer_id = trainers.trainer_id
+    ON trainer_battles.trainer_1 = trainers.trainer_id AND trainer_battles.trainer_2 = trainers.trainer_id AND trainer_battles.winner = trainers.trainer_id
 GROUP BY trainer_name
 ORDER BY battle_date DESC;
