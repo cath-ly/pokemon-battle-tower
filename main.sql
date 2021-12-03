fb2c456ff3070713e28ef383a7616eaaf37b4b4d
NickA42 <nicholas.alicea@centre.edu>
2021-12-02 19:22:22 -0500

    yo

--- main.sql
+++ main.sql
@@ -4,14 +4,14 @@ DROP DATABASE IF EXISTS battle_towers_pers;
 
 CREATE DATABASE battle_towers_pers;
 USE battle_towers_pers;
-/*
+
 SOURCE TypeList_CRUD/crud_typelist.sql;
 SOURCE MoveList_CRUD/crud_movelist.sql;
 SOURCE typead_CRUD/crud_typead.sql;
 SOURCE typedisad_CRUD/crud_typedisad.sql;
 SOURCE Species_CRUD/crud_species.sql;
 SOURCE speciestype_CRUD/crud_speciestype.sql;
-*/
+
 SOURCE Trainers_CRUD/crud_trainer.sql;
 SOURCE Pokemon_CRUD/crud_pokemon.sql;
 SOURCE PokeMove_CRUD/crud_pokemove.sql;
