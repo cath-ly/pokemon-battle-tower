8c26270c56c742eec5916f17cb42fb7174c7890c
xxtakrakxx <chantakrak.ath-ly@centre.edu>
2021-12-03 20:43:21 +0000

    okay heres a model

--- restrictMoveType.sql
+++ restrictMoveType.sql
@@ -1,1 +1,23 @@
 -- restrictMoveType.sql
+-- try to restrict like the lab!!!
+CREATE TRIGGER restrictMoveType
+BEFORE INSERT ON move_type FOR EACH ROW
+
+;
+
+DROP FUNCTION IF EXISTS count_party;
+CREATE FUNCTION count_party(t_id INT)
+RETURNS INT
+RETURN(
+    SELECT COUNT(t_id) FROM trainers_party_members
+    WHERE trainer_id = t_id
+);
+-- i love ligma
+DELIMITER //
+CREATE OR REPLACE TRIGGER max_party
+    BEFORE INSERT ON trainers_pokemon FOR EACH ROW
+    IF (count_party(trainer_id)) >= 6 THEN
+        -- error here
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Too many Pokemon/max party';
+END IF;
+//
--- restrictPokemonTypes.sql
+++ restrictPokemonTypes.sql
@@ -1,1 +1,17 @@
 -- restrictPokemonTypes.sql
+-- try to restrict like the lab!!!
+-- we need to count types of movetypes should only be 1
+
+DROP FUNCTION IF EXISTS count_poketype;
+CREATE FUNCTION count_poketype(s_name CHAR)
+RETURNS INT
+RETURN(
+    SELECT COUNT(s_name) FROM species_type
+    WHERE species_name = s_name
+);
+
+CREATE OR REPLACE TRIGGER restrictPokeType
+BEFORE INSERT ON species_type FOR EACH ROW
+IF (count_poketype(species_name)) > 2 THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Unable to add pokemon, maximum party size reached.";
+;
