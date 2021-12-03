29c78dd26bac0850adf3b17613646bedf00b68c3
NickA42 <nicholas.alicea@centre.edu>
2021-12-02 23:28:46 -0500

    fixed some more errors

--- TrainersAwards_CRUD/add_TrainersAwards.sql
+++ TrainersAwards_CRUD/add_TrainersAwards.sql
@@ -1,3 +1,3 @@
-INSERT INTO trainers_awards(trainer_id, award_name)
+INSERT INTO trainer_awards(trainer_id, award_name)
 VALUES  ('1', 'Bloodthirsty'), 
         ('1', 'First Tenth');
--- TrainersAwards_CRUD/create_TrainersAwards.sql
+++ TrainersAwards_CRUD/create_TrainersAwards.sql
@@ -1,7 +1,7 @@
 CREATE TABLE trainer_awards(
 	PRIMARY KEY (trainer_id, award_name), 
 	trainer_id INT,
-	award_name VARCHAR(12),
+	award_name VARCHAR(30),
 	FOREIGN KEY (trainer_id) REFERENCES trainers(trainer_id), 
 	FOREIGN KEY (award_name) REFERENCES awards(award_name)
 );
